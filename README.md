fbf58818382382e1112668dd3b7ce00043dae3ba
Jake Rieger <contact.jakerieger@gmail.com>
2026-01-22 17:49:18 -0500

    0.1.0 update

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ Locations that need to be searched and deleted:
 - `~\AppData\Local\Native Instruments\Kontakt 8\LibrariesCache`
 - `~\AppData\Local\Native Instruments\Kontakt 8\komplete.db3`
 
+## Install
+
+You can download the latest version of K8-LRT from the [releases](https://github.com/jakerieger/K8-LRT/releases/latest) page.
+
+## Changelog
+
+### 0.1.0 (January 22, 2026)
+
+- Initial release of K8-LRT
+
 ## Building
 
 K8-LRT is written in pure C using the Windows API. It can be compiled via `nmake` which is included with the MSVC toolchain.
--- src/main.c
+++ src/main.c
@@ -1,20 +1,115 @@
 #include <stdio.h>
 #include <windows.h>
+#include <winerror.h>
+#include <winnt.h>
 #include <winreg.h>
+#include <shlobj.h>
+#include <Shlwapi.h>
+#include <winuser.h>
+
+//====================================================================//
+//                   -- UI ELEMENT DEFINITIONS --                     //
+//====================================================================//
 
 #define IDC_LISTBOX 101
 #define IDC_REMOVE_BUTTON 102
 #define IDC_QUIT_BUTTON 103
+#define IDC_CHECKBOX_BACKUP 104
+
+#define IDI_APPICON 501
 
 HWND h_listbox;
 HWND h_remove_button;
 HWND h_quit_button;
+HWND h_backup_checkbox;  // Whether or not we should backup delete files
+
+//====================================================================//
+//                          -- GLOBALS --                             //
+//====================================================================//
 
 #define MAX_LIB_COUNT 256
 
 char* g_libraries[MAX_LIB_COUNT];
 int g_lib_count      = 0;
 int g_selected_index = -1;
+BOOL g_backup_files  = TRUE;
+
+#define LIB_CACHE_ROOT "Native Instruments\\Kontakt 8\\LibrariesCache"
+#define DB3_ROOT "Native Instruments\\Kontakt 8\\komplete.db3"
+
+//====================================================================//
+//                      -- HELPER FUNCTIONS --                        //
+//====================================================================//
+
+char* get_local_appdata_path() {
+    PWSTR psz_path = NULL;
+
+    HRESULT hr = SHGetKnownFolderPath(&FOLDERID_LocalAppData, 0, NULL, &psz_path);
+    if (SUCCEEDED(hr)) {
+        // convert to ansi
+        char path[MAX_PATH];
+        WideCharToMultiByte(CP_ACP, 0, psz_path, -1, path, MAX_PATH, NULL, NULL);
+        CoTaskMemFree(psz_path);
+        return _strdup(path);
+    } else {
+        fprintf(stderr, "Failed to retrieve path. Error code: 0x%08X\n", (UINT)hr);
+    }
+
+    return NULL;
+}
+
+char* join_str(const char* prefix, const char* suffix) {
+    const size_t prefix_len = strlen(prefix);
+    const size_t suffix_len = strlen(suffix);
+    const size_t needed     = prefix_len + suffix_len + 1;
+
+    char* buffer = (char*)malloc(needed);
+    int offset   = 0;
+    memcpy(buffer, prefix, prefix_len);
+    offset += prefix_len;
+    memcpy(buffer + offset, suffix, suffix_len);
+    offset += suffix_len;
+    buffer[offset] = '\0';
+
+    return buffer;
+}
+
+char* join_paths(const char* path_prefix, const char* path_suffix) {
+    char* prefix = join_str(path_prefix, "\\");
+    char* full   = join_str(prefix, path_suffix);
+    free(prefix);
+    return full;
+}
+
+void attach_console() {
+    if (AttachConsole(ATTACH_PARENT_PROCESS)) {
+        FILE* f_dummy;
+        freopen_s(&f_dummy, "CONOUT$", "w", stdout);
+        freopen_s(&f_dummy, "CONOUT$", "w", stderr);
+    }
+}
+
+BOOL file_exists(const char* path) {
+    DWORD dw_attrib = GetFileAttributesA(path);
+    return (dw_attrib != INVALID_FILE_ATTRIBUTES && !(dw_attrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+// Enable registry key backups
+void enable_backup_privilege() {
+    HANDLE h_token;
+    TOKEN_PRIVILEGES tp;
+    LUID luid;
+
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &h_token)) {
+        if (LookupPrivilegeValue(NULL, SE_BACKUP_NAME, &luid)) {
+            tp.PrivilegeCount           = 1;
+            tp.Privileges[0].Luid       = luid;
+            tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+            AdjustTokenPrivileges(h_token, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL);
+        }
+        CloseHandle(h_token);
+    }
+}
 
 void reset_libraries() {
     for (int i = 0; i < g_lib_count; i++) {
@@ -69,18 +164,92 @@ void remove_selected_library(HWND hwnd) {
     LPCSTR base_path = "SOFTWARE\\Native Instruments";
     HKEY h_key;
 
-    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, base_path, 0, KEY_WRITE, &h_key) == ERROR_SUCCESS) {
+    // 1. Remove registry key
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, base_path, 0, KEY_ALL_ACCESS, &h_key) == ERROR_SUCCESS) {
+        if (g_backup_files) {
+            HKEY h_subkey;
+            if (RegOpenKeyExA(HKEY_LOCAL_MACHINE,
+                              join_paths(base_path, g_libraries[g_selected_index]),
+                              0,
+                              KEY_READ,
+                              &h_subkey) == ERROR_SUCCESS) {
+                char* reg_filename = join_str(g_libraries[g_selected_index], ".reg");
+                DeleteFileA(reg_filename);
+                LONG backup_res = RegSaveKeyExA(h_subkey, reg_filename, NULL, REG_LATEST_FORMAT);
+
+                if (backup_res != ERROR_SUCCESS) {
+                    char err_msg[128];
+                    sprintf_s(err_msg, 128, "Backup failed. Error: %ld", backup_res);
+                    MessageBox(hwnd, err_msg, "Error", MB_ICONERROR);
+                    exit(1);
+                }
+
+                RegCloseKey(h_subkey);
+                free(reg_filename);
+            }
+        }
+
         LONG res = RegDeleteKeyA(h_key, g_libraries[g_selected_index]);
-        if (res == ERROR_SUCCESS) {
-            MessageBox(hwnd, "Library removed from registry.", "Success", MB_OK);
-            reset_libraries();  // Reset UI
-        } else {
+        if (res != ERROR_SUCCESS) {
             MessageBox(hwnd, "Failed to delete key. Are you Admin?", "Error", MB_ICONERROR);
+            exit(1);
         }
 
         RegCloseKey(h_key);
     }
+
+    // 2. Check for .xml files in `C:\Program Files\Common Files\Native Instruments\Service Center`
+    char* prefix =
+      join_paths("C:\\Program Files\\Common Files\\Native Instruments\\Service Center", g_libraries[g_selected_index]);
+    char* filename = join_str(prefix, ".xml");
+
+    if (file_exists(filename)) {
+        if (g_backup_files) {
+            char* bak_filename = join_str(prefix, ".xml.bak");
+            if (!CopyFileExA(filename, bak_filename, NULL, NULL, NULL, 0)) {
+                MessageBox(hwnd, "Failed to backup .xml file.", "Error", MB_ICONERROR);
+                exit(1);
+            }
+            free(bak_filename);
+        }
+
+        if (!DeleteFileA(filename)) {
+            MessageBox(hwnd, "Failed to delete .xml file. Are you Admin?", "Error", MB_ICONERROR);
+            exit(1);
+        }
+    }
+    free(prefix);
+
+    // 3. Check for cache file in `~\AppData\Local\Native Instruments\Kontakt 8\LibrariesCache`
+    // TODO: This one is tough. Cache files are binary formats and filenames appear to be hashes of some kind.
+
+    // 4. Create backup of `~\AppData\Local\Native Instruments\Kontakt 8\komplete.db3` to force DB rebuild (if enabled)
+    char* appdata_local = get_local_appdata_path();
+    char* db3           = join_paths(appdata_local, "Native Instruments\\Kontakt 8\\komplete.db3");
+    free(appdata_local);
+
+    if (file_exists(db3)) {
+        if (g_backup_files) {
+            char* db3_bak = join_str(db3, ".bak");
+            if (!CopyFileExA(db3, db3_bak, NULL, NULL, NULL, 0)) {
+                MessageBox(hwnd, "Failed to backup `komplete.db3`.", "Error", MB_ICONERROR);
+                exit(1);
+            }
+            free(db3_bak);
+        }
+
+        if (!DeleteFileA(db3)) {
+            MessageBox(hwnd, "Failed to delete `komplete.db3`. Are you Admin?", "Error", MB_ICONERROR);
+            exit(1);
+        }
+    }
+
+    MessageBox(hwnd, "Successfully delete library ``", "Success", MB_OK | MB_ICONINFORMATION);
 }
+
+//====================================================================//
+//                      -- WINDOW CALLBACK --                         //
+//====================================================================//
 
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
     switch (umsg) {
@@ -104,11 +273,24 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
                                        10,
                                        26,
                                        265,
-                                       250,
+                                       240,
                                        hwnd,
                                        (HMENU)IDC_LISTBOX,
                                        GetModuleHandle(NULL),
                                        NULL);
+
+            h_backup_checkbox = CreateWindowEx(0,
+                                               "BUTTON",
+                                               "Backup files before deleting",
+                                               WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
+                                               10,
+                                               250,
+                                               220,
+                                               20,
+                                               hwnd,
+                                               (HMENU)IDC_CHECKBOX_BACKUP,
+                                               GetModuleHandle(NULL),
+                                               NULL);
 
             h_quit_button = CreateWindowEx(WS_EX_CLIENTEDGE,
                                            "BUTTON",
@@ -151,9 +333,13 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
                                       DEFAULT_PITCH | FF_DONTCARE,
                                       "Segoe UI");
 
+            SendMessage(h_label, WM_SETFONT, (WPARAM)h_font, TRUE);
             SendMessage(h_listbox, WM_SETFONT, (WPARAM)h_font, TRUE);
             SendMessage(h_quit_button, WM_SETFONT, (WPARAM)h_font, TRUE);
             SendMessage(h_remove_button, WM_SETFONT, (WPARAM)h_font, TRUE);
+
+            SendMessage(h_backup_checkbox, WM_SETFONT, (WPARAM)h_font, TRUE);
+            SendMessage(h_backup_checkbox, BM_SETCHECK, (WPARAM)TRUE, TRUE);
 
             // Search registry for key entries in `HKEY_LOCAL_MACHINE/SOFTWARE/Native Instruments/..`
             query_ni_registry_keys(hwnd);
@@ -163,9 +349,16 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
 
         case WM_COMMAND: {
             switch (LOWORD(wparam)) {
-                case IDC_QUIT_BUTTON:
-                    PostMessage(hwnd, WM_CLOSE, 0, 0);
+                case IDC_QUIT_BUTTON: {
+                    int response = MessageBox(hwnd,
+                                              "Are you sure you want to quit?",
+                                              "Confirm Quit",
+                                              MB_YESNO | MB_ICONWARNING | MB_DEFBUTTON2);
+                    if (response == IDYES) {
+                        SendMessage(hwnd, WM_CLOSE, 0, 0);
+                    }
                     break;
+                }
 
                 case IDC_REMOVE_BUTTON: {
                     if (g_selected_index != -1) {
@@ -179,7 +372,9 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
                           MessageBox(hwnd, message, "Confirm Removal", MB_YESNO | MB_ICONWARNING | MB_DEFBUTTON2);
 
                         if (response == IDYES) {
+                            g_backup_files = (SendMessage(h_backup_checkbox, BM_GETCHECK, 0, 0) == BST_CHECKED);
                             remove_selected_library(hwnd);
+                            reset_libraries();
                         }
                     } else {
                         MessageBox(hwnd, "Please select a library first.", "No Selection", MB_OK);
@@ -203,16 +398,25 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
             return 0;
         }
 
-        case WM_DESTROY:
+        case WM_DESTROY: {
             PostQuitMessage(0);
             return 0;
+        }
     }
 
     return DefWindowProc(hwnd, umsg, wparam, lparam);
 }
 
+//====================================================================//
+//                         -- ENTRYPOINT --                           //
+//====================================================================//
+
 int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR lp_cmd_line, int n_cmd_show) {
-    // create_and_attach_console();
+#ifndef NDEBUG
+    attach_console();
+#endif
+
+    enable_backup_privilege();
 
     const char CLASS_NAME[] = "K8RemovalWindowClass";
 
@@ -221,6 +425,7 @@ int WINAPI WinMain(HINSTANCE h_instance, HINSTANCE h_prev_instance, LPSTR lp_cmd
     wc.hInstance     = h_instance;
     wc.lpszClassName = CLASS_NAME;
     wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon         = LoadIcon(h_instance, MAKEINTRESOURCE(IDI_APPICON));
 
     RegisterClass(&wc);
 
